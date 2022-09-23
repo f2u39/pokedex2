9b017144a8a017217f056b47ee1dfc14c294c517
f2u39 <unifuusawatari@gmail.com>
2022-09-23 16:19:48 +0900

    Initial Commit

---
+++ pokedex/pokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokedexApp.swift
+//  pokedex
+//
+//  Created by Fuu on 2022/09/23.
+//
+
+import SwiftUI
+
+@main
+struct pokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
